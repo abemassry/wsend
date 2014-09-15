5a8f6700b684a759a02387572dde378039fa4861
Phip Jhon <phip@riseup.net>
2014-09-14 21:08:59 -0400

    fix broken github links

--- update.sh
+++ update.sh
@@ -2,11 +2,11 @@
 
 wsend_base=$HOME/.wsend
 
-curl -L -s -o $wsend_base/wsend https://raw.github.com/abemassry/wsend/master/wsend
+curl -L -s -o $wsend_base/wsend https://raw.githubusercontent.com/abemassry/wsend/master/wsend
 chmod +x $wsend_base/wsend
-curl -L -s -o $wsend_base/README.md https://raw.github.com/abemassry/wsend/master/README.md
-curl -L -s -o $wsend_base/COPYING https://raw.github.com/abemassry/wsend/master/COPYING
-curl -L -s -o $wsend_base/version https://raw.github.com/abemassry/wsend/master/version
+curl -L -s -o $wsend_base/README.md https://raw.githubusercontent.com/abemassry/wsend/master/README.md
+curl -L -s -o $wsend_base/COPYING https://raw.githubusercontent.com/abemassry/wsend/master/COPYING
+curl -L -s -o $wsend_base/version https://raw.githubusercontent.com/abemassry/wsend/master/version
 
 #calling back update version
 $wsend_base/wsend $@
